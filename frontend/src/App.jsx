f32251378f7e4572b447e3e46cc36e0bef54f146
Shailesh <shaileshhawale004@gmail.com>
2025-08-16 19:52:17 +0530

    Frontend Demo

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,7 @@ import { Header } from "./components";
 import { Home } from "./components/pages";
 import Footer from "./components/Footer";
 import { RouterProvider } from "react-router-dom";
-import router from "./components/Router";
+import router from "./components/router";
 import Todo from "./components/todo";
 
 function App() {
